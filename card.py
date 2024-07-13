a9924b160fccf6d6f2ad511054727a90845088f5
Álvaro <vegaromeroalvaro@gmail.com>
2024-07-13 17:04:02 +0200

    remove suit in card class

--- card.py
+++ card.py
@@ -1,15 +1,14 @@
 class Card:
-    def __init__(self, rank, suit):
+    def __init__(self, rank):
         self.rank = rank
-        self.suit = suit
 
     def value(self):
         if self.rank in ['J', 'Q', 'K']:
             return 10
         elif self.rank == 'A':
-            return 11  # This can also be 1, depending on the Hand's total
+            return 11  # 1 or 11, decided in Hand class
         else:
             return int(self.rank)
 
     def __str__(self):
-        return f'{self.rank} of {self.suit}'
+        return f'{self.rank}'
