535ce96026c844eebd5500bbf6bb477ca57d16e0
Álvaro <vegaromeroalvaro@gmail.com>
2024-07-13 18:03:29 +0200

    change implementation of Hand Class

--- hand.py
+++ hand.py
@@ -4,10 +4,13 @@ class Hand:
 
     def value(self):
         total = sum(card.value() for card in self.cards)
-        # If total value is over 21 and there's an Ace in the hand, reduce total by 10
-        if total > 21 and any(card.rank == 'A' for card in self.cards):
-            return total - 10
-        return total
+        aces = sum(card.rank == 'A' for card in self.cards)
 
+        while total > 21 and aces:
+            total -= 10
+            aces -= 1
+
+        return total
+    
     def __str__(self):
         return ', '.join(str(card) for card in self.cards)
